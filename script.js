63add1ba009558380dc13105b1bdda88d5bb57bd
piekario13 <piekario13@gmail.com>
2018-07-01 16:12:14 +0200

    carousel

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+	let carouselList = $("#carousel ul");
+  setInterval(changeSlide,3000);
+  function changeSlide(){
+    carouselList.animate({marginLeft:-400}, 400, placeFirstSlideAfterLast)
+  }
+  function placeFirstSlideAfterLast(){
+    carouselList.find("li:last").after(carouselList.find("li:first"));
+    carouselList.css({marginLeft:0});
+  };
+});
